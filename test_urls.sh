ca903597980e0045dea79563256dfece46329f1f
Devin Handspiker-Wade <devin@handspiker-wade.com>
2026-03-10 15:50:46 -0300

    Update test script to make it easier to see failures

---
+++ test_urls.sh
@@ -0,0 +1,29 @@
+PROXY_HOST="${PROXY_HOST:-localhost:3142}"
+
+function test_request() {
+    local red_text=$'\033[0;31m'
+    local reset_text=$'\033[0m'
+
+    curl --output /dev/null -s -x "http://${PROXY_HOST}" "$1"
+    RET=$?
+    if [ "$RET" -eq "0" ]
+    then
+        echo -ne "PASS:\t"
+    else
+        echo -ne "${red_text}FAIL:\t"
+    fi
+    echo -e "$1 ${reset_text}"
+
+    return $RET
+}
+
+# Docker passthrough 
+test_request http://download.docker.com/linux/debian/dists/trixie/pool/stable/amd64/containerd.io_1.7.27-1_amd64.deb
+# Standard debian
+test_request http://deb.debian.org/debian/pool/main/n/nano/nano-tiny_5.4-2+deb11u3_amd64.deb
+# security debian
+test_request http://security.debian.org/debian-security/pool/main/f/fish/fish_3.1.2-3+deb11u1_amd64.deb
+# Debian mirror as an "unknown repo"
+test_request http://mirror.csclub.uwaterloo.ca/debian/pool/main/v/vim/vim_9.1.2141-1_amd64.deb
+# Proxmox and Release file 
+test_request http://download.proxmox.com/debian/ceph-squid/dists/trixie/InRelease
