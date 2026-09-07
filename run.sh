ebd21161265d77e85d175cc4630eeffa96834556
Devin Handspiker-Wade <devin@handspiker-wade.com>
2026-09-06 21:54:04 -0300

    Added APT config into the container to use itself

--- run.sh
+++ run.sh
@@ -3,4 +3,10 @@ set -e
 haproxy -f /etc/haproxy/haproxy.cfg &
 tinyproxy -c /etc/tinyproxy/tinyproxy.conf &
 
+if [ ! -f /etc/apt/apt.conf.d/apt-cacher-dt.conf ]; then
+    echo 'Acquire::http::Proxy "http://127.0.0.1:3142";' > /etc/apt/apt.conf.d/apt-cacher-dt.conf
+    echo 'Dir::Cache "";' >> /etc/apt/apt.conf.d/apt-cacher-dt.conf
+    echo 'Dir::Cache::archives "";' >> /etc/apt/apt.conf.d/apt-cacher-dt.conf
+fi
+
 exec /docker-entrypoint.sh "$@"
